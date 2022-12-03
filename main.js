7bd8032964cf16c237af8ab845f8a1f49e59fd8c
Renan Vaz <renan.piresvz@gmail.com>
2022-12-03 19:13:01 -0300

    Preview opcoes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log("vamo");
 
 const options = [
-  { src: "imgs/IMG_2521.PNG", type: "eyes" },
-  { src: "imgs/IMG_2510.PNG", type: "top" },
+  { src: "imgs/IMG_2521.PNG", type: "eyes", offset: -35 },
+  { src: "imgs/IMG_2510.PNG", type: "top", offset: -25 },
 ];
 
 const $canvas = document.querySelector(".canvas");
@@ -10,11 +10,20 @@ const $options = document.querySelector(".options__list");
 
 $options.appendChild;
 
-options.forEach((o, i) => {
+options.forEach((o) => {
   const $li = document.createElement("li");
   const $button = document.createElement("button");
 
-  $button.textContent = i;
+  const $img = document.createElement("img");
+
+  $img.className = "option__img";
+  $img.src = o.src;
+  $img.width = 100;
+  $img.style.transform = `translate(-50%, ${o.offset}px)`;
+
+  $button.appendChild($img);
+  $button.className = "option";
+  $button.style.backgroundImage = `url(${o.src})`;
   $button.addEventListener("click", () => {
     const $img = document.createElement("img");
 
